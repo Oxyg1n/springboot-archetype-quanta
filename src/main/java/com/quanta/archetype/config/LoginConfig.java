20cff0916a59378d98b3ebaf1e325fd22c678842
linine <2498808430@qq.com>
2023-09-07 16:41:34 +0800

    fix : 修复拦截器返回失败跨域处理失效的问题
    Over

--- src/main/java/com/quanta/archetype/config/LoginConfig.java
+++ src/main/java/com/quanta/archetype/config/LoginConfig.java
@@ -1,11 +1,17 @@
 package com.quanta.archetype.config;
 
 import com.quanta.archetype.interceptor.AuthInterceptor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
 
 /**
  * 登录拦截器配置
@@ -24,14 +30,19 @@ public class LoginConfig implements WebMvcConfigurer {
     /**
      * 解决跨域问题
      */
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("*")
-                .allowedMethods("GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowCredentials(true)
-                .maxAge(3600)
-                .allowedHeaders("*");
+    @Bean
+    FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("*"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+        corsConfiguration.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        registrationBean.setFilter(new CorsFilter(source));
+        registrationBean.setOrder(-1);//filter 0 1 自然顺序，-1表示优先执行
+        return registrationBean;
     }
 
     @Override
